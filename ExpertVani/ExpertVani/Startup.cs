dd9f4c524101a2587e5d9a5499fc4f616353b82a
Swapnil Awasthi <swapnil.awasthi9@gmail.com>
2018-01-16 23:55:57 +0530

    New project

---
+++ ExpertVani/ExpertVani/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExpertVani.Startup))]
+namespace ExpertVani
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
